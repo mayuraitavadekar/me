b94137f7c79c28da36f308d31db21e6c520717da
Mayur Aitavadekar <mayur.aitavadekar@viit.ac.in>
2020-03-17 02:01:03 +0530

    Updates

---
+++ precache-manifest.ca8c63f452f3a5d639a85df714717bbd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5928da58ac57b181c613ef7c91cf2598",
+    "url": "/me/index.html"
+  },
+  {
+    "revision": "4235d90316baea1c4f6c",
+    "url": "/me/static/css/2.7ad897a5.chunk.css"
+  },
+  {
+    "revision": "1bd982c1ba1625b1face",
+    "url": "/me/static/css/main.c7c767ca.chunk.css"
+  },
+  {
+    "revision": "4235d90316baea1c4f6c",
+    "url": "/me/static/js/2.b505c383.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/me/static/js/2.b505c383.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bd982c1ba1625b1face",
+    "url": "/me/static/js/main.20e488de.chunk.js"
+  },
+  {
+    "revision": "99e1a3f98b9d66b3b95e",
+    "url": "/me/static/js/runtime-main.6954d1b8.js"
+  }
+]);
